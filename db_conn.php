e8251176ec20ea7256f31734549d211400724933
gabrielitapipoca <134813714+gabrielitapipoca@users.noreply.github.com>
2023-06-26 17:37:58 -0300

    betacast

---
+++ db_conn.php
@@ -0,0 +1,6 @@
+<?php  
+/* ball */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'italvain_opensb');
+define('DB_PASSWORD', 'HuhDuhSixHundgeos');
+define('DB_NAME', 'italvain_pedrotube');
